b8a8ada4c856a6dfd3cd556b0f559e809cb680d0
Ohanefk <ohanefk@gmail.com>
2019-12-02 13:46:52 +0600

    2

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -133,4 +133,10 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER =
+EMAIL_HOST_PASSWORD = 
